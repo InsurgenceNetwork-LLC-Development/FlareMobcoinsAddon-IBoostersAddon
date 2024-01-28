4f28c3e64972e7e49e331608117b7f151e31d1bf
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 19:26:08 -0500

    api jar update

--- src/main/java/com/insurgencedev/flaremobcoinsaddon/FlareMobCoinsAddon.java
+++ src/main/java/com/insurgencedev/flaremobcoinsaddon/FlareMobCoinsAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.flaremobcoinsaddon;
 
 import com.insurgencedev.flaremobcoinsaddon.listeners.FlareMobCoinEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "FlareMobcoinsAddon", version = "1.0.1", author = "InsurgenceDev", description = "Flare Mobcoins Support")
 public class FlareMobCoinsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("TMMobCoins")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("TMMobCoins")) {
             registerEvent(new FlareMobCoinEventListener());
         }
     }
