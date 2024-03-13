48edb51513c2783c1ea657aa6750367e9b37255d
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-03-13 18:48:52 -0500

    version bump + api update

--- src/main/java/com/insurgencedev/flaremobcoinsaddon/listeners/FlareMobCoinEventListener.java
+++ src/main/java/com/insurgencedev/flaremobcoinsaddon/listeners/FlareMobCoinEventListener.java
@@ -30,6 +30,6 @@ public final class FlareMobCoinEventListener implements Listener {
     }
 
     private double calculateAmount(double amount, double multi) {
-        return amount * (multi < 1 ? 1 + multi : multi);
+        return amount * (multi <= 1 ? 1 + multi : multi);
     }
 }
